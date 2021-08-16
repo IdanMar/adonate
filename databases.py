bdd20a01116f381a026d86506cb28cd513170b51
IdanMar <89003785+IdanMar@users.noreply.github.com>
2021-08-16 09:40:16 +0300

    Created databases.py

---
+++ databases.py
@@ -0,0 +1,9 @@
+from model import Base, User
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///users.db?check_same_thread=False')
+Base.metadata.create_all(engine)
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
